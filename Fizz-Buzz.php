b36bb6d6d6e778ffa0a939d23699bbe4cda406b4
Natapone Charsombut <dong@abctech-thailand.com>
2011-11-30 10:01:33 +0700

    add Buzz function

--- Fizz-Buzz.php
+++ Fizz-Buzz.php
@@ -2,6 +2,7 @@
     function fizz_buzz($input) {
         # init
         $fizz_int = 3;
+        $buzz_int = 5;
         
         # find Greatest common divisor
         $gcd = $fizz_int;
@@ -9,6 +10,7 @@
         # set of operations
         $operations = array(
                 $fizz_int   => 'Fizz',
+                $buzz_int   => 'Buzz',
         );
         
         # calculate
