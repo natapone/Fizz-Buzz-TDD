e97f289ebea15ec0c192da098815175fc5ea2324
Natapone Charsombut <dong@abctech-thailand.com>
2011-11-30 09:58:47 +0700

    add fail test Buzz

--- TestFizzBuzzTdd.php
+++ TestFizzBuzzTdd.php
@@ -9,12 +9,27 @@ class TestFizzBuzz extends PHPUnit_Framework_TestCase {
     public function testFizz($ans, $input) {
         $this->assertEquals($ans, fizz_buzz($input),'Fizz fail');
     }
-
+    
+    /**
+     * @dataProvider providerBuzz
+     */
+    public function testBuzz($ans, $input) {
+        $this->assertEquals($ans, fizz_buzz($input),'Buzz fail');
+    }
+    
     public function providerFizz() {
         return array(
             array('Fizz', 3),
             array('Fizz', 9),
             array('Fizz', 36),
+        );
+    }
+    
+    public function providerBuzz() {
+        return array(
+            array('Buzz', 5),
+            array('Buzz', 10),
+            array('Buzz', 100),
         );
     }
 }
