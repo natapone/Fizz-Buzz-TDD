a3cc38c1480539ba44d56507fbf620f5fc03222e
Natapone Charsombut <dong@abctech-thailand.com>
2011-11-30 09:54:11 +0700

    add Fizz function

--- Fizz-Buzz.php
+++ Fizz-Buzz.php
@@ -1,7 +1,23 @@
 <?php
     function fizz_buzz($input) {
+        # init
+        $fizz_int = 3;
         
-        $ans = $input;
+        # find Greatest common divisor
+        $gcd = $fizz_int;
+        
+        # set of operations
+        $operations = array(
+                $fizz_int   => 'Fizz',
+        );
+        
+        # calculate
+        foreach ($operations as $operation => $ans_str) {
+            if($input%$operation == 0 and $input != 0) {
+                $ans = $ans_str;
+                break;
+            }
+        }
         
         return $ans;
     }
